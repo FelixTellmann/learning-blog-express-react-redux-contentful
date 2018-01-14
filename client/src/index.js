91a81246f8bd70eb0f32f73fe95d9c45b5f93101
Felix Tellmann <felix@tellmann.co.za>
2018-01-14 21:55:37 +0200

    Initial Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, {render} from 'react-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+
+import {BrowserRouter as Router} from "react-router-dom";
+import store from "./store";
+import {Provider} from 'react-redux';
+
+render(
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById('root')
+);
